6d981691b9d61b59114afca7f6d90696846260e4
Ayoub Kaanich <kayoub5@live.com>
2016-06-23 17:19:25 +0100

    No clean

--- provision.sh
+++ provision.sh
@@ -1,30 +1,44 @@
 #!/bin/sh
 
 cd /
+# -----------------
+# install languages
+# -----------------
 
 apk update
 apk add --no-cache \
+    go \
     nodejs \
     python \
     python-dev \
     py-pip \
-    go
 
+# ---------------
+# install linters
+# ---------------
 gem install scss_lint -v 0.48.0
-gem sources -c
-
-pip --no-cache-dir install yamllint==1.2.1 proselint==0.5.3
-apk del py-pip
 
 go get -u github.com/alecthomas/gometalinter
 gometalinter --install --update
-go clean all
-apk del go
-rm -Rf /usr/lib/go
-rm -Rf /gopath/src
-rm -Rf /gopath/pkg
 
 npm -q install
+
+pip --no-cache-dir install yamllint==1.2.1 proselint==0.5.3
+
+# -------------
+# clean sources
+# -------------
+gem sources -c
+#go clean all
+#rm -Rf /gopath/src
+#rm -Rf /gopath/pkg
+#rm -Rf /usr/lib/go
+
 npm cache clean
+apk del python-dev
+
+# -------------
+# create target
+# -------------
 
 mkdir -p /src
