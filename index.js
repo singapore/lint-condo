910f3eb000deee7df62df2fc008830eb209fe72a
Ayoub Kaanich <kayoub5@live.com>
2016-06-23 13:43:36 +0100

    Use colors

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
 /* eslint-disable no-sync */
-/* eslint-disable no-console */
 
 var fs = require('fs');
+var chalk = require('chalk');
 var yaml = require('js-yaml');
 var logSymbols = require('log-symbols');
-var lintDependencies = require('/usr/src/lint-condo/package.json').dependencies;
 var spawn = require('child_process').spawn;
+var pkg = require('./package.json');
 
-var lintPackages = Object.keys(lintDependencies);
-lintPackages.push('markdownlint'); // npm markdownlint-cli
-lintPackages.push('yamllint'); // pip
-lintPackages.push('proselint'); // pip
-lintPackages.push('scss-lint'); // gem
+var lintPackages = Object.keys(pkg.dependencies).concat(
+  'markdownlint', // npm markdownlint-cli
+  'yamllint', // pip
+  'proselint', // pip
+  'scss-lint' // gem
+);
+
+/* eslint-disable no-console */
+var logger = {
+  error: function() {
+    console.error(chalk.bgReg.apply(null, arguments));
+  },
+  info: function() {
+    console.log(chalk.blue.apply(null, arguments));
+  },
+  pass: function() {
+    console.log(logSymbols.success, chalk.green.apply(null, arguments));
+  },
+  fail: function() {
+    console.log(logSymbols.error, chalk.red.apply(null, arguments));
+  }
+};
+/* eslint-enable no-console */
+
 var configFile = null;
-
 try {
   configFile = fs.readFileSync('lint-condo.yaml', 'utf-8');
 } catch (err1) {
   try {
     configFile = fs.readFileSync('.lint-condo.yaml', 'utf-8');
   } catch (err2) {
-    console.error('ERROR: Couldn\'t find a config file');
+    logger.error('ERROR: Couldn\'t find a config file');
     process.exit(1);
   }
 }
@@ -41,7 +59,7 @@ config.linters.forEach(function(linter) {
 
 queue
   .then(function() {
-    console.log('\nSummary:');
+    logger.info('\nSummary:');
     Object.keys(statuses).forEach(function(command) {
       var printedCommand = command;
       var firstWord = command.substr(0, command.indexOf(' '));
@@ -49,28 +67,28 @@ queue
         printedCommand = firstWord;
       }
       if (statuses[command] === 0) {
-        console.log(logSymbols.success, printedCommand);
+        logger.pass(printedCommand);
       } else {
-        console.log(logSymbols.error, printedCommand);
+        logger.fail(printedCommand);
       }
     });
     process.exit(exitCode);
   })
   .catch(function(err) {
-    console.error(err.stack);
+    logger.error(err.stack);
     process.exit(1);
   });
 
 function run(command) {
   return new Promise(function(resolve) {
-    console.log('\nRunning "%s"', command);
+    logger.info('\nRunning "%s"', command);
     var child = spawn('/bin/sh', ['-c', command], {stdio: 'inherit'});
     child.on('error', function(err) {
-      console.error(err);
+      logger.error(err);
       resolve(255); // eslint-disable-line no-magic-numbers
     });
     child.on('exit', function(code) {
-      console.log('\nFinished "%s" (code=%d)\n', command, code);
+      logger.info('\nFinished "%s" (code=%d)\n', command, code);
       resolve(code);
     });
   });
