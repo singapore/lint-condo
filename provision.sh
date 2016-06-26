62a78cdf8951615e0d32b1eea3f95eb409ba52c8
Ayoub Kaanich <kayoub5@live.com>
2016-06-26 05:43:44 +0100

    alpine no ruby (#51)
    Closes #16 
    Closes #50 
    * Support Go linters
    
    * gopath
    
    * fix env order
    
    * No clean
    
    * Add git
    
    * clean go
    
    * Use alpine 3.3
    
    * drop help
    
    * ruby-bundler
    
    * bash
    
    * no docs
    
    * no bundler

---
+++ provision.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+cd /
+# -----------------
+# install languages
+# -----------------
+
+apk update
+apk add --no-cache \
+    git \
+    go \
+    nodejs=4.3.0-r0 \
+    python \
+    python-dev \
+    py-pip \
+    ruby
+
+# ---------------
+# install linters
+# ---------------
+gem install scss_lint -v 0.48.0 -N
+
+go get -u github.com/alecthomas/gometalinter
+gometalinter --install --update
+
+npm -q install
+
+pip --no-cache-dir install yamllint==1.2.1 proselint==0.5.3
+
+# -------------
+# clean sources
+# -------------
+gem sources -c
+rm -Rf /gopath/src
+rm -Rf /gopath/pkg
+rm -Rf /usr/lib/go
+
+npm cache clean
+apk del python-dev git go
+
+# -------------
+# create target
+# -------------
+
+mkdir -p /src
