0928d9f50e8d63a630c30785b36fafbf77997374
Rhys Arkins <rhys@keylocation.sg>
2016-10-26 06:41:44 +0200

    Upgrade eslint-config-google to 0.7.0 (#161)
    * Upgrade eslint-config-google to 0.7.0
    
    * Update index.js
    
    * Drop var
    
    * Update index.js
    
    * Update .eslintrc.yml
    
    * Update index.js
    
    * Update index.js
    
    * Update fibonacci.js

--- test/fixture/fibonacci.js
+++ test/fixture/fibonacci.js
@@ -1,10 +1,10 @@
 
 module.exports = function fibonacci(n) {
-  var fib = []; // Initialize array!
+  const fib = []; // Initialize array!
 
   fib[0] = 0;
   fib[1] = 1;
-  for (var i = 2; i <= n; i++) {
+  for (let i = 2; i <= n; i++) {
     // Next fibonacci number = previous + one before previous
     // Translated to JavaScript:
     fib[i] = fib[i - 2] + fib[i - 1];
