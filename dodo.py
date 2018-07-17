5b96e402bebb71bf266a61e2035c010a01988627
Ayoub Kaanich <kayoub5@live.com>
2018-07-17 19:36:57 +0200

    Add hadolint (#412)

--- dodo.py
+++ dodo.py
@@ -1,6 +1,6 @@
 
 from doit.tools import run_once
-import os
+from os.path import abspath
 
 DOIT_CONFIG = {
     'backend': 'sqlite3',
@@ -20,4 +20,31 @@ def make_test(name, target, cmd=''):
   }
 
 def task_test():
-  yield make_test('self', os.getcwd())
+  yield make_test('self', abspath('.'))
+
+def task_hadolint():
+  rm_repo = 'rm -Rf hadolint/repo'
+  yield {
+    'name': 'repo',
+    'actions': [
+      rm_repo,
+      'mkdir -p hadolint/repo',
+      'cd hadolint/repo && curl -L https://github.com/lukasmartinelli/hadolint/archive/v1.0.tar.gz | tar xz --strip-components=1'
+    ],
+    'targets': ['hadolint/repo'],
+    'uptodate': [True],
+    'clean': [rm_repo]
+  }
+
+  rm_bin = 'rm -Rf hadolint/bin'
+  yield {
+    'name': 'build',
+    'task_dep': ['hadolint:repo'],
+    'actions': [
+      rm_bin,
+      'mkdir -p hadolint/bin',
+      'docker run -v {target}:/hadolint/ mitchty/alpine-ghc:7.10 bash /hadolint/build.sh'.format(target=abspath('hadolint'))
+    ],
+    'targets': ['hadolint/bin'],
+    'clean': [rm_bin]
+  }
---
+++ hadolint/Dockerfile
@@ -0,0 +1,8 @@
+
+# https://github.com/mitchty/alpine-linux-ghc-bootstrap
+FROM mitchty/alpine-ghc:8.0
+
+RUN cabal update && stack update
+
+# Install some stuff
+RUN cabal --version
